debf7e1bcc397b5dc1a5d4b1f55d198f2c157900
Rita Mernissi <rita.mernissi@hotmail.com>
2018-10-09 21:33:31 +0200

    update button, update pizza, update list - DOESNT update the price yet

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,34 @@
+//Ajoute ou supprime pepperonni
+// Bouton "Marche/Arrêt"
+// Ajoute peperroni dans le panier mais n'update pas encore le prix
+$(".btn-pepperonni").click(function(){
+    $(".btn-pepperonni").toggleClass("active");
+    $(".pep").toggle();
+    $(".price > ul li:nth-child(1)").toggle();
+});
+// Ajoute ou supprime mushrooms
+$(".btn-mushrooms").click(function(){
+    $(".btn-mushrooms").toggleClass("active");
+    $(".mushroom").toggle();
+    $(".price > ul li:nth-child(2)").toggle();
+});
+// Ajoute ou supprime green-peppers
+$(".btn-green-peppers").click(function(){
+    $(".btn-green-peppers").toggleClass("active");
+    $(".green-pepper").toggle();
+    $(".price > ul li:nth-child(3)").toggle();
+});
+ // Regular Sauce or White Sauce
+$(".btn-sauce").click(function(){
+    
+    $(".btn-sauce").toggleClass("active");
+    $(".sauce").toggleClass("sauce-white");
+    $(".price > ul li:nth-child(4)").toggle();
+});
+// Crust or Gluten Free
+$(".btn-crust").click(function(){
+    $(".btn-crust").toggleClass("active");
+    $(".crust").toggleClass("crust-gluten-free");
+    $(".price > ul li:nth-child(5)").toggle();
+});
 // Write your Pizza Builder JavaScript in this file.
